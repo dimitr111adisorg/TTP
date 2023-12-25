7f2a73e09424ad0b8523018aa036059cb2d77d33
dimitrananouri <93881589+dimitrananouri@users.noreply.github.com>
2023-12-25 15:52:25 +0200

    log in, sign up, change password (with db connection)

--- LogInSignInChangePassword.java
+++ LogInSignInChangePassword.java
@@ -1,122 +1,140 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
+
 public class LogInSignInChangePassword {
     
-    public static void chooseWhatToDo() throws Exception {
-        
-        System.out.println("Press 1 to log in, 2 to sign in, 3 to change password");
+    public static void chooseWhatToDo() {
+        System.out.println("Press: \n1 to log in, \n2 to sign in, \n3 to change password");
         Scanner a = new Scanner(System.in);
-        int answer = a.nextInt();
-        
-        try {
-            if (answer == 1) {
-            
-            LogInSignInChangePassword.logIn();
-        } else if (answer == 2) {
-            
-            LogInSignInChangePassword.signIn();
-            
-        } else if (answer == 3) {
-            LogInSignInChangePassword.changePassword();
-        } else {
-            System.out.println("Please enter a valid value");
-            LogInSignInChangePassword.chooseWhatToDo();
-        }
-        } catch (Exception e) {
-            System.out.println("Please enter valid value");
-            LogInSignInChangePassword.chooseWhatToDo();
-            // TODO: handle exception
-        }
-        /*if (answer == 1) {
-            
-            LogInSignInChangePassword.logIn();
-        } else if (answer == 2) {
-            
-            LogInSignInChangePassword.signIn();
-            
-        } else if (answer == 3) {
-            LogInSignInChangePassword.changePassword();
-        } else {
-            System.out.println("Please enter a valid value");
-            LogInSignInChangePassword.chooseWhatToDo();
-        }*/
-    }
-    
-    public static void logIn() throws Exception {
-        
-        boolean check = true;
-        while (check == true) {
-            
-            
-            System.out.println("Please enter username");
-            Scanner b = new Scanner(System.in);
-            String usernameLogIn = b.nextLine();
-            System.out.println("Please enter password. ONLY NUMBERS PLEASE!!"); 
-            Scanner c = new Scanner(System.in);
-            int passwordLogIn = c.nextInt();
-            //String encryptedUsername = encrypt(usernameLogIn);
-            if ( usernameLogIn == "hi")/*(usernameLogIn =! με τη βάση || passwordLogIn =! με τη βάση )*/  {
-                System.out.println("Wrong or unfind username or password try again");
-                LogInSignInChangePassword.logIn();
+        String answer = a.nextLine();
+        boolean flag = true;
+        do {
+            if (answer.equals("1")) {
+                flag = false;
+                logIn();
+            } else if (answer.equals("2")) {
+                 flag = false;
+                signIn();
+            } else if (answer.equals("3")) {
+                 flag = false;
+                changePassword();
             } else {
-                System.out.println("Welcome!");
-                Menu a = new Menu(); 
-                Menu.menu();
+                System.out.println("Please enter a valid answer (1,2 or 3)");
+                answer = a.nextLine();
             }
-        }
+        } while (flag);
+        a.close();       
     }
-    
-    public static void signIn() throws Exception {
-        
-        boolean check = true;
-        while (check == true) {
-            
-            System.out.println("Please enter username");
-            Scanner b = new Scanner(System.in);
-            String usernameSignIn = b.nextLine();
-            System.out.println("Please enter password. ONLY NUMBERS PLEASE!!"); 
-            Scanner c = new Scanner(System.in);
-            int passwordSignIn = c.nextInt();
-            //String encryptedUsername = encrypt(usernameLogIn);
-            if ( usernameSignIn == "hi")/*(usernameLogIn == με τη βάση)*/  {
-                System.out.println("Username already in use");
-                LogInSignInChangePassword.signIn();
-            } else {
-                System.out.println("Thank you for your registration. We are redirecting you to menu");
-                Menu a = new Menu(); 
-                Menu.menu();
+
+    private static void logIn() {
+        Scanner b = new Scanner(System.in);
+        boolean flag = true;
+        do {
+            System.out.println("Please type your username");
+            String username = b.nextLine();
+            System.out.println("Please type your password");
+            String password = b.nextLine();
+            String sql = "Select * from Users Where username = ? and password = ?";
+            try (Connection conn = Connect.connect();
+                    PreparedStatement pstmt  = conn.prepareStatement(sql)){
+                    pstmt.setString(1, username);
+                    pstmt.setString(2, password);
+                    ResultSet rs = pstmt.executeQuery();
+                    if (rs.next()) {
+                        flag = false;
+                        User currentUser = new User(username, password);
+                        System.out.println("Welcome back, "+currentUser.getUsername()+"!");
+                    } else {
+                        System.out.println("Wrong username or password. Try again");
+                    }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
             }
-        }
+        } while (flag);
+        b.close();    
     }
-    
-    public static void changePassword() throws Exception {
-        
-        boolean check = true;
-        while (check == true) {
-            
-            System.out.println("Please enter username");
-            Scanner b = new Scanner(System.in);
-            String usernameChange = b.nextLine();
-            System.out.println("Please enter new password. ONLY NUMBERS PLEASE!!"); 
-            Scanner c = new Scanner(System.in);
-            int passwordChange = c.nextInt();
-            System.out.println("Please verify new password. ONLY NUMBERS PLEASE!!"); 
-            Scanner d = new Scanner(System.in);
-            int passwordChangeVerify = d.nextInt();
-            //String encryptedUsername = encrypt(usernameLogIn);
-            if ( usernameChange == "hi")/*(usernameLogIn =! δεν υπαρχει στη βαση)*/  {
-                System.out.println("Wrong or unfind username try again");
-                LogInSignInChangePassword.changePassword();
-            } else if (passwordChange != passwordChangeVerify) {
-                System.out.println("The password and verify password do not match");
-                LogInSignInChangePassword.changePassword();
-            } else {
-                System.out.println("Your password has change! We are redirecting you to the menu");
-                Menu a = new Menu(); 
-                Menu.menu();
-            }
+
+    private static void signIn() {
+        Scanner c = new Scanner(System.in);
+        System.out.println("Please enter a username:");
+        String username = c.nextLine();
+        boolean flag = true;
+        do {
+           String sql = "Select * From Users Where username = ?";
+            try (Connection conn = Connect.connect();
+                PreparedStatement pstmt  = conn.prepareStatement(sql)){
+                pstmt.setString(1, username);
+                ResultSet rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    System.out.println("This username already exists! Please choose something else!");
+                    username = c.nextLine();
+                } else {
+                    flag = false;
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            } 
+        } while (flag);
+        System.out.println("Please enter your password");
+        String password = c.nextLine();
+        String sql2 = "Insert into Users (username, password) Values (?,?)";
+        try (Connection conn = Connect.connect();
+            PreparedStatement pstmt  = conn.prepareStatement(sql2)){
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.executeUpdate();
+            User currentuUser = new User(username, password);
+            System.out.println("Welcome, "+currentuUser.getUsername());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } 
+        c.close();
+    }
+
+    private static void changePassword() {
+        Scanner d = new Scanner(System.in);
+        System.out.println("Please enter your username:");
+        String username = d.nextLine();
+        System.out.println("Please enter your current password:");
+        String password = d.nextLine();
+        boolean flag = true;
+        do {
+           String sql = "Select * From Users Where username = ? and password = ?";
+            try (Connection conn = Connect.connect();
+                PreparedStatement pstmt  = conn.prepareStatement(sql)){
+                pstmt.setString(1, username);
+                pstmt.setString(2, password);
+                ResultSet rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    flag = false;
+                } else {
+                    System.out.println("Wrong username or password. Try again!");
+                    System.out.println("Please enter your username:");
+                    username = d.nextLine();
+                    System.out.println("Please enter your current password:");
+                    password = d.nextLine();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            } 
+        } while (flag);
+        System.out.println("Type your new password");
+        String newPassword = d.nextLine();
+        String sql2 = "UPDATE Users SET password=? WHERE username=?;";
+        try (Connection conn = Connect.connect();
+            PreparedStatement pstmt  = conn.prepareStatement(sql2)) {
+            pstmt.setString(1, username);
+            pstmt.setString(2, newPassword);
+            pstmt.executeUpdate();
+            User currentUser = new User(username, newPassword);
+            System.out.println(currentUser.getUsername()+",your password has been changed successfully!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
+
+        d.close();
     }
 }
