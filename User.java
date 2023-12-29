61d125f9fd5a9e9bd4cf034c92be0248c9083348
dimitrananouri <93881589+dimitrananouri@users.noreply.github.com>
2023-12-30 01:49:12 +0200

    user class

--- User.java
+++ User.java
@@ -2,6 +2,8 @@ public class User {
 
     String username;
     String password;
+
+    public User () {}
     
     public User (String username, String password) {
         this.username = username;
