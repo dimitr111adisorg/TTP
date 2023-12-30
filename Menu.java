9de214740fe94d0736487f929cf8e2837b09fe7f
AfroditeDim <104227652+AfroditeDim@users.noreply.github.com>
2023-12-30 20:55:08 +0200

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -5,7 +5,7 @@ public class Menu {
         
         boolean check = true; 
         while (check == true) {
-            System.out.println("Press 1 to translate, 2 to learn, 3 to see everything you have save. Press 0 to exit");
+            System.out.println("Press 1 to translate, 2 to learn, 3 to see everything you have saved. Press 0 to exit");
             Scanner in = new Scanner(System.in);
             int answer =in.nextInt();
             if (answer == 2) {
@@ -14,15 +14,18 @@ public class Menu {
                 
             } else if ( answer == 3) {
                 System.out.println("Press 4 to see the saved words and 5 to see the saved questions");
-                /*Scanner on = new Scanner(System.in);
+                Scanner on = new Scanner(System.in);
                 int answer2 = on.nextInt();
                 if (answer2 == 4) {
-                    TranslationHistory = new TranslationHistory();
-                    TranslationHistory.displayHistory();
+                    TranslationHistory translationHistory = new TranslationHistory();
+                    translationHistory.displayHistory();
                     
                 } else if (answer2 == 5) {
+                    QuestionsList.showSavedQuestions("1");
+                    QuestionsList.showSavedQuestions("2");
+                    QuestionsList.showSavedQuestions("3");
                     
-                }*/
+                }
             } else if (answer == 1) {
                 Translator b = new Translator();
                 Translator.translate();
