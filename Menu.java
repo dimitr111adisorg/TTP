ff7c9e76410751d89959ca2e734ea0c04f9c03c6
dimitrananouri <93881589+dimitrananouri@users.noreply.github.com>
2024-01-05 16:46:44 +0200

    main menu

--- Menu.java
+++ Menu.java
@@ -1,39 +1,40 @@
 import java.util.Scanner;
+
 public class Menu {
-    
-   public static void main(String [] args ) throws Exception {
 
-        LogInSignUpChangePassword.chooseWhatToDo();
+    public static void menu(String username) throws Exception {
 
-        boolean check = true; 
+        boolean check = true;
         while (check == true) {
-            System.out.println("Press 1 to translate, \n2 to learn, \n3 to see everything you have saved. \nPress 0 to exit");
+            System.out.println("\nPress: \n1 To Translate, \n2 To Learn, \n3 To see your saved words/questions \nPress 0 to exit");
             Scanner in = new Scanner(System.in);
-            int answer =in.nextInt();
-            if (answer == 2) {
-                Learning a = new Learning();
-                Learning.MultipleChoice();
-                
-            } else if ( answer == 3) {
-                System.out.println("Press 4 to see the saved words and 5 to see the saved questions");
+            String answer =in.nextLine();
+            if (answer.equals("1")) {
+                Translator.translate(username);
+            } else if (answer.equals("2")) {
+                Learning.MultipleChoice(username);
+            } else if (answer.equals("3")) {
+                System.out.println("\nPress 4 to see the saved words and 5 to see the saved questions\nPress 0 to go back to menu");
                 Scanner on = new Scanner(System.in);
-                int answer2 = on.nextInt();
-                if (answer2 == 4) {
-                    TranslationHistory translationHistory = new TranslationHistory();
-                    translationHistory.displayHistory();
-                    
-                } else if (answer2 == 5) {
-                    QuestionsList.showSavedQuestions("username");
-                }
-                
-            } else if (answer == 1) {
-                Translator.translate();
-            } else if (answer == 0) {
-                System.out.println("See you next time"); 
+                String answer2 = on.nextLine();
+                if (answer2.equals("4")) {
+                    SavedWords.showSavedWords(username);
+                 } else if (answer2.equals("5")) {
+                    QuestionsList.showSavedQuestions(username);
+                 } else if (answer2.equals("0")) {
+                    menu(username);
+                 } else {
+                    System.out.println("Please enter a valid answer (4 or 5)");
+                    answer2 = in.nextLine();
+                 }
+            } else if (answer.equals("0")) {
+                System.out.println("See you next time!");
                 check = false;
+                System.exit(0);
             } else {
-                System.out.println("Enter a valid value");
+                System.out.println("Please enter a valid answer (0, 1, 2, or 3)");
+                answer = in.nextLine();
             }
         }
-    }       
+    } 
 }
