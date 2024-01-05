665ebd2ee8d1fb911ef652e0018e4e5d27c5055d
dimitrananouri <93881589+dimitrananouri@users.noreply.github.com>
2024-01-05 16:52:07 +0200

    words class

---
+++ Words.java
@@ -0,0 +1,19 @@
+public class Words {
+    
+    String text;
+    String translation;
+
+    public Words(String text, String translation) {
+        this.text = text;
+        this.translation = translation;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+    
+}
